585fb003b793ff350eae84a41f5ae8d193768909
mccleskeyc <mccleskeyc@gmail.com>
2020-12-17 00:59:14 -0500

    commit tree d8c34d7ed150efae3730d85e0846895251a20af3

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Claire"
+  end
+
+  get '/hometown' do
+    "My hometown is Haymarket, VA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is anything by the Jonas Brothers."
+  end
+
 end
